789768b7a85123ae5fbb8492398db1aa586114c9
luqmanzakariya <luqman.zakariya@gmail.com>
2022-04-18 04:33:04 +0700

    update next.config.js

--- next.config.js
+++ next.config.js
@@ -11,6 +11,11 @@ const nextConfig = withPWA({
     register: true,
     skipWaiting: true,
     disable: process.env.NODE_ENV === "development"
+  },
+  experimental: {
+    amp: {
+      skipValidation: true
+    }
   }
 })
 
